aad2b1222a8a29fcdbf4085c58a9b7a33592bd83
Siddhant Gujrathi <115349105+SiddhantGujrathi1@users.noreply.github.com>
2024-03-02 22:48:09 +0530

    Update sssss.js

--- sssss.js
+++ sssss.js
@@ -1,7 +1,7 @@
-var typed = new typed(".text", {
+var typed = new Typed(".text", {
     strings:["Frontend Developer","Android Developer","Data Scientist"],
     typeSpeed:100,
-    backSpeed:100,
+    backSpeed:30,
     backDelay:1000,
     loop:true
 });
